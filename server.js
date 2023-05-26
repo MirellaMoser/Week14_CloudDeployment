434eda40821c28aa12d1a8883e4cfa890e3d024a
MirellaMoser <mirella.moser@gmx.net>
2023-05-26 10:49:19 +0200

    test1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course, its me, Mirella');
+    res.send('Hello DevOps Course. This is the first amendment.');
 });
 
 app.listen(PORT, HOST);
