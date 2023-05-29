80b9d39f7cc40b6dea560d8da55f689860fd4d4a
MirellaMoser <mirella.moser@gmx.net>
2023-05-29 19:58:54 +0200

    changes server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course. This is amendment n1.');
+    res.send('Hello DevOps Course. This is amendment number two.');
 });
 
 app.listen(PORT, HOST);
